7d9523491117a3b1647c840e50ff99d7f1b0150d
luan.dbrito <luandb90@gmail.com>
2018-05-29 17:09:50 -0300

    Initial commit

---
+++ app/src/main/java/br/senac/sp/l13/homelogin/CadastroActivity.java
@@ -0,0 +1,13 @@
+package br.senac.sp.l13.homelogin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cadastro_layout);
+    }
+}
---
+++ app/src/main/java/br/senac/sp/l13/homelogin/Cadastro_Activity.java
@@ -0,0 +1,13 @@
+package br.senac.sp.l13.homelogin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Cadastro_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cadastramento_activity);
+    }
+}
